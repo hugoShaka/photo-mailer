3bf089191e815d35b702d9fc59e639d4cd4039ae
Shaka <hugo.hervieux@rez-gif.supelec.fr>
2016-02-28 23:15:09 +0100

    Correction de nombreuses fautes de frappe+débug Ajout des self dans les methodes Catch de l'exeption "No ITPC info" Début de la fonction main Mise en place d'un environnement de logging décent pour iptc Modifications esthetiques Passage des input en raw_input Différenciation entre le scan du dossier et la création des mails

--- mailerv2.py
+++ mailerv2.py
@@ -7,38 +7,57 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication 
-from lib.iptcinfo import IPTCInfo
+from iptcinfo import IPTCInfo
+import logging
+
+logging.basicConfig(filename='error.log')
 
 class photo:
   """A photo can be sent, it also contains the recipient's email address"""
-  def __init__(self,fileloc,mailaccount,message):
+  def __init__(self,fileloc):
     self.location=fileloc
-    self.info=IPTCInfo(path_to_image)
-    self.addr=str(self.info.data['object name'])
+    try:
+      self.info=IPTCInfo(self.location)
+      self.addr=str(self.info.data['object name'])
+      self.IPTCred=True
+    except Exception:
+      print(fileloc+" No valid IPTC tags found.")
+      self.IPTCred=False
+
+  def generateEmail(self,mailaccount,message):
     if self.addr=='':
       self.email=None
+      print("Warning no valid email address for : "+self.location)
     else:
       self.email=mail(self.addr,self.location,mailaccount,message)
     self.sent=False
-  def send()
+
+  def send(self):
     if self.sent:
       print('Warning :Re-sending the email')
     self.email.send()
     self.sent=True
 
 class folder:
-  """Représente un dossier"""
+  """Contains the path to the photos. Can be scanned to get the photos and can be used to mass edit/send the mails/the photos"""
   def __init__(self,pathtofolder):
     self.path=pathtofolder
     self.files=[]
-  def scan():
-    howmanyimport=0
+  def scan(self):
+    howManyImport=0
     for root, dirs, files in os.walk(self.path):
       for file in files:
         if file.endswith(".jpg"):
-          self.files.append(photo(os.path.join(root, file)))
-          howmanyimport+=1
-    print(howmanyimport + "files were sucessfully imported")
+          importedPhoto=photo(os.path.join(root, file))
+          if importedPhoto.IPTCred:
+            self.files.append(importedPhoto)
+            howManyImport+=1
+    print(str(howManyImport) + " files were sucessfully imported")
+  def generateEmails(self,mailaccount,message):
+    for pic in self.files:
+      pic.generateEmail(mailaccount,message)
+
+
 
 class mailaccount:
   """Compte mail et parametres de connexion au serveur
@@ -51,23 +70,24 @@ class mailaccount:
     self.login="None"
     self.connected=False
 
-  def credentials():
-    self.login=input("Login")
-    self.pwd=getpass.getpass("Mot de passe")
+  def credentials(self):
+    self.login=raw_input("Login : ")
+    self.pwd=getpass.getpass("Mot de passe : \n")
 
-  def showMailSettings():
+  def showMailSettings(self):
+    print("\n--------------------\n MailServer Settings \n--------------------\n")
     print("sender : "+self.sender+"\nsmtp server : "+self.smtp+"\nport : "+self.port+"\nlogin : ")
     if not self.connected:
-      print("Not connected")
+      print("Status : not connected")
     else:
-      print("Connected as : "+self.login)
+      print("Status : connected as : "+self.login)
 
-  def editMailSettings():
-    self.sender=input("sender ?")
-    self.smtp=input("server ?")
-    self.port=input("port?")
+  def editMailSettings(self):
+    self.sender=raw_input("sender ?")
+    self.smtp=raw_input("server ?")
+    self.port=raw_input("port?")
 
-  def log():
+  def log(self):
     try:
       self.mailserver=smtplib.SMTP(self.smtp,self.port)
       self.mailserver.ehlo()
@@ -75,28 +95,32 @@ class mailaccount:
       self.mailserver.ehlo()
       self.mailserver.login(self.login,self.pwd)
       self.connected=True
-    except (socket.error) as err
-      print("Socket error:.({0}): {1}".format(e.errno, e.strerror)
+    except (socket.error) as err:
+      print("Socket error:.({0}): {1}".format(e.errno, e.strerror))
       self.connected=False
 
-  def unlog()
-    	self.mailserver.quit()
-        self.connected=False
+  def unlog(self):
+    self.mailserver.quit()
+    self.connected=False
 
 class mail:
   """Objet mail qui possède les methodes pour etre envoye, recupere ses parametres d'un objet mailaccount"""
   def __init__(self,reciever,photo,mailaccount,message):
-    self.msg=MIMEMultipart()
-    self.msg['From'] = mailaccount.sender
-    self.msg['To'] = reciever
-    self.msg['Subject'] = mailaccount.subject
-    self.msg.attach(MIMEText(message.create()))
+    if reciever==None:
+      print("\n /!\ Email not created due to invalid email address")
+    else:
+      self.msg=MIMEMultipart()
+      self.msg['From'] = mailaccount.sender
+      self.msg['To'] = reciever
+      self.msg['Subject'] = message.subject
+      self.msg.attach(MIMEText(message.generate()))
     
-    pj=open(photo, 'rb')
-    self.msg.attach(MIMEApplication(pj.read(),Content_Disposition='attachement;filename="%s"' % os.path.basename(photo),Name=os.path.basename(photo)))
-    pj.close()
+      pj=open(photo, 'rb')
+      self.msg.attach(MIMEApplication(pj.read(),Content_Disposition='attachement;filename="%s"' % os.path.basename(photo),Name=os.path.basename(photo)))
+      pj.close()
+      print("Mail to : "+reciever+" successfully generated")
 
-  def send():
+  def send(self):
     """Send the mail object"""
     if (mailaccount.connected):
       mailaccount.mailserver.sendmail(mailaccount.sender, self.msg['To'], self.msg.as_string())
@@ -105,28 +129,33 @@ class mail:
       mailaccount.mailserver.sendmail(mailaccount.sender, self.msg['To'], self.msg.as_string())
       
 class message:
-    """A class to manage the e-mail text"""
+  """A class to manage the e-mail text"""
   def __init__(self,text='No text'):
     self.text=text
     self.sign=''
-  def create():
+    self.subject='Your photo'
+  def generate(self):
     return self.text+'\n-- \n'+self.sign
 
 def main():
   print("Not yet")
   mailacc=mailaccount()
   mailacc.showMailSettings()
-  if (input("Edit mail server settings ? (y/N)")=='y')
-    mailacc.edit():
+  if (raw_input("\nEdit mail server settings ? (y/N)")=='y'):
+    mailacc.editMailSettings()
   print("Please enter your credentials")
   mailacc.credentials()
   print("Testing the settings")
   mailacc.log()
-  if mailacc.conncted
-    print("Successfully logged :) ")
+  if mailacc.connected:
+    print("\nSuccessfully logged    :) \n")
   else:
     print("Exiting")
-    break
-  pathto=input("Choose yout folder")
-  location=folder(pathto)
-  
+  pathto=raw_input("Choose your folder")
+  currentFolder=folder('/home/shaka/Downloads/photos/')
+  currentFolder.scan()
+  currentMessage=message()
+  currentMessage.text=raw_input("enter the email's body text")
+  currentFolder.generateEmails(mailacc,currentMessage)
+
+main()
